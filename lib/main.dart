a73bf64c3138a7b968afa4a25d8ab5c8aa38737d
seoyoung1013 <lemi01@naver.com>
2022-05-04 13:28:35 +0900

    counting

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: MyHomePage(title: 'Fluter Demo Home Page'));
+        home: MyHomePage(title: 'Flutter Demo Home Page'));
   }
 }
 
